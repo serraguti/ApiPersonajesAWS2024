10796d2d5a8a4ceeb40a5be0494ba974a888d23f
serraguti <pacoserranox@gmail.com>
2024-05-16 11:18:36 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,42 @@
+﻿using ApiPersonajesAWS.Models;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private RepositoryPersonajes repo;
+
+        public PersonajesController(RepositoryPersonajes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Personaje>>>
+            GetPersonajes()
+        {
+            return await this.repo.GetPersonajesAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personaje>>
+            Get(int id)
+        {
+            return await this.repo.FindPersonajeAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>
+            Create(Personaje personaje)
+        {
+            await this.repo.CreatePersonajeAsync
+                (personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+    }
+}
